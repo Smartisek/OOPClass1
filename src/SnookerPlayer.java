04d7ca72cd2e7e0540a838874b2ed9b95cee8685
Smartisek <domalipdominik@gmail.com>
2023-09-22 15:05:01 +0100

    Adding increase prize money function into class

--- src/SnookerPlayer.java
+++ src/SnookerPlayer.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class SnookerPlayer {
+public class SnookerPlayer {
+    private String name;
+    private String nationality;
+    private double prizeMoney;
+    private Integer numberOfGames;
+    private Integer numberOfWonGames;
+
+    public SnookerPlayer(String name, String nationality, double prizeMoney, Integer numberOfGames, Integer numberOfWonGames) {
+        this.name = name;
+        this.nationality = nationality;
+        this.prizeMoney = prizeMoney;
+        this.numberOfGames = numberOfGames;
+        this.numberOfWonGames = numberOfWonGames;
+    }
+    @Override
+    public String toString() {
+        return "SnookerPlayer{" +
+                "name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", prizeMoney=" + prizeMoney +
+                ", numberOfGames=" + numberOfGames +
+                ", numberOfWonGames=" + numberOfWonGames +
+                '}';
+    }
+
+    public void increasePrizeMoney(double amount){
+        prizeMoney = prizeMoney + amount;
+    }
 }
